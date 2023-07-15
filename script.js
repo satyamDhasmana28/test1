7f45c0996e4633444101d86aec52423ba6819420
Your Name <satyam.dhasmanaa@gmail.com>
2023-07-15 21:21:52 +0530

    first

--- script.js
+++ script.js
@@ -8,20 +8,45 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map((item)=>{
+    if(item.profession === 'developer'){
+      console.log(item);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((item)=>{
+    if(item.profession === 'developer'){
+      console.log(item);
+    }
+  })
 }
 
 function addData() {
   //Write your code here, just console.log
+  let intern = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(intern);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let nonAdminArr = arr.filter((item)=>{
+    return item.profession!=='admin';
+  });
+  console.log(nonAdminArr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let anotherArray = [
+    { id: 4, name: "satyam", age: "23", profession: "developer" },
+    { id: 5, name: "ayush", age: "24", profession: "developer" },
+    { id: 6, name: "rahul", age: "125", profession: "developer" },
+  ];
+
+  arr =arr.concat(anotherArray);
+  console.log(arr);
 }
